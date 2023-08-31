6d2cb7e64b3caebd475f6107e12ee06557bd5e8c
Luiz Matias <luiz.matias1999@gmail.com>
2023-08-31 04:38:52 -0300

    feat: added failing test

--- src/test/java/com/luizmatias/findadev/FindADevApplicationTests.java
+++ src/test/java/com/luizmatias/findadev/FindADevApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class FindADevApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		throw new Exception();
 	}
 
 }
