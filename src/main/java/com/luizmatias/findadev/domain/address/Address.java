a0d6f453ec35bcb6da942ad0515dda7ea85555a7
Luiz Matias <luiz.matias1999@gmail.com>
2023-08-31 18:29:36 -0300

    feat: refactored city to be complete address

---
+++ src/main/java/com/luizmatias/findadev/domain/address/Address.java
@@ -0,0 +1,32 @@
+package com.luizmatias.findadev.domain.address;
+
+import com.luizmatias.findadev.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "addresses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    private Double latitude;
+    private Double longitude;
+    @OneToOne(mappedBy = "address")
+    private User user;
+
+}
