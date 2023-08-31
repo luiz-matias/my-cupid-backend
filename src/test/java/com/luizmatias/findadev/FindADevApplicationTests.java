e48fe4735cc69dc1a4df5622dcfc3510da2c3609
Luiz Matias <luiz.matias1999@gmail.com>
2023-08-31 03:42:24 -0300

    fix: added classes into class annotation

--- src/test/java/com/luizmatias/findadev/FindADevApplicationTests.java
+++ src/test/java/com/luizmatias/findadev/FindADevApplicationTests.java
@@ -3,7 +3,7 @@ package com.luizmatias.findadev;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FindADevApplicationTests.class)
 class FindADevApplicationTests {
 
 	@Test
