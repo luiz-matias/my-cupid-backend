8bc52d3eed1a86301b5c7a1c8905c7b123ef3979
Luiz Matias <luiz.matias1999@gmail.com>
2023-08-31 02:32:56 -0300

    feat: creation of city class

---
+++ src/main/java/com/luizmatias/findadev/city/City.java
@@ -0,0 +1,26 @@
+package com.luizmatias.findadev.city;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cities")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class City {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String state;
+
+}
