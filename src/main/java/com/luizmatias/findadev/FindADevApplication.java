b3a2a746db6c73dd54fb022dfef821f493296245
Luiz Matias <luiz.matias1999@gmail.com>
2023-08-31 00:53:35 -0300

    feat: project initial configuration

---
+++ src/main/java/com/luizmatias/findadev/FindADevApplication.java
@@ -0,0 +1,13 @@
+package com.luizmatias.findadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindADevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindADevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizmatias/findadev/FindADevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizmatias.findadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindADevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
