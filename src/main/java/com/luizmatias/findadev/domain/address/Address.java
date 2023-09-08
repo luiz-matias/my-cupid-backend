81aed20838776355a7e1dbec49f1769c2525da76
Luiz Matias <luiz.matias1999@gmail.com>
2023-09-08 20:23:25 -0300

    fix: Fixed import dependency

--- src/main/java/com/luizmatias/findadev/domain/address/Address.java
+++ src/main/java/com/luizmatias/findadev/domain/address/Address.java
@@ -1,9 +1,7 @@
 package com.luizmatias.findadev.domain.address;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.luizmatias.findadev.domain.entities.User;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
