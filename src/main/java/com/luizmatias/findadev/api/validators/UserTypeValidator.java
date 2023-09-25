d6661f7125dcc57b526b792d7ebeedabdf0a1f12
Luiz Matias <luiz.matias1999@gmail.com>
2023-09-25 19:44:35 -0300

    feat: added user role validators

--- src/main/java/com/luizmatias/findadev/api/validators/UserTypeValidator.java
+++ src/main/java/com/luizmatias/findadev/api/validators/UserTypeValidator.java
@@ -3,9 +3,6 @@ package com.luizmatias.findadev.api.validators;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import static com.luizmatias.findadev.api.dtos.mappers.UserTypeMapper.USER_TYPE_CLIENT;
-import static com.luizmatias.findadev.api.dtos.mappers.UserTypeMapper.USER_TYPE_DEVELOPER;
-
 public class UserTypeValidator implements ConstraintValidator<UserType, String> {
 
     @Override
@@ -14,7 +11,10 @@ public class UserTypeValidator implements ConstraintValidator<UserType, String> 
 
     @Override
     public boolean isValid(String userTypeField, ConstraintValidatorContext cxt) {
-        return userTypeField.equalsIgnoreCase(USER_TYPE_CLIENT) || userTypeField.equalsIgnoreCase(USER_TYPE_DEVELOPER);
+        return userTypeField != null && (
+                userTypeField.equalsIgnoreCase(com.luizmatias.findadev.domain.entities.UserType.CLIENT.getType()) ||
+                        userTypeField.equalsIgnoreCase(com.luizmatias.findadev.domain.entities.UserType.DEVELOPER.getType())
+        );
     }
 
 }
