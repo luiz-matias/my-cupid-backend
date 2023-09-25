b2ea136f86bb3278b6a7884e0577eba255668824
Luiz Matias <luiz.matias1999@gmail.com>
2023-09-25 19:48:47 -0300

    feat: created JWT token service

---
+++ src/main/java/com/luizmatias/findadev/api/security/TokenService.java
@@ -0,0 +1,41 @@
+package com.luizmatias.findadev.api.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.luizmatias.findadev.db.models.UserEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("{api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(UserEntity user) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        return JWT.create()
+                .withIssuer("auth-api")
+                .withSubject(user.getEmail())
+                .withExpiresAt(getExpirationDate())
+                .sign(algorithm);
+    }
+
+    public String validateToken(String token) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        return JWT.require(algorithm)
+                .withIssuer("auth-api")
+                .build()
+                .verify(token)
+                .getSubject();
+    }
+
+    private Instant getExpirationDate() {
+        return LocalDateTime.now().plusMinutes(1).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
