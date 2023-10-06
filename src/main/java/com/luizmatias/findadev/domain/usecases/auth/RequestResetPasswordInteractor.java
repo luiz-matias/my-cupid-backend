4310f6cd6846d2b34bc05d3e820ef39fd33ba3de
Luiz Matias <luiz.matias1999@gmail.com>
2023-10-06 18:42:33 -0300

    feat: refactored password reset usecases

--- src/main/java/com/luizmatias/findadev/domain/usecases/auth/RequestResetPasswordInteractor.java
+++ src/main/java/com/luizmatias/findadev/domain/usecases/auth/RequestResetPasswordInteractor.java
@@ -1,45 +1,35 @@
 package com.luizmatias.findadev.domain.usecases.auth;
 
-import com.luizmatias.findadev.domain.entities.PasswordToken;
 import com.luizmatias.findadev.domain.entities.User;
+import com.luizmatias.findadev.domain.exceptions.FailedToSendEmailException;
 import com.luizmatias.findadev.domain.exceptions.InvalidTokenException;
 import com.luizmatias.findadev.domain.exceptions.ResourceNotFoundException;
+import com.luizmatias.findadev.domain.repositories.EmailSenderRepository;
 import com.luizmatias.findadev.domain.repositories.PasswordEncoder;
-import com.luizmatias.findadev.domain.repositories.PasswordTokenRepository;
 import com.luizmatias.findadev.domain.repositories.UserRepository;
-
-import java.util.Date;
-import java.util.Optional;
 
 public class RequestResetPasswordInteractor {
 
     private final UserRepository userRepository;
-    private final PasswordTokenRepository passwordTokenRepository;
+    private final VerifyUserTemporaryTokenInteractor verifyUserTemporaryTokenInteractor;
+    private final EmailSenderRepository emailSenderRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public RequestResetPasswordInteractor(UserRepository userRepository, PasswordTokenRepository passwordTokenRepository, PasswordEncoder passwordEncoder) {
+    public RequestResetPasswordInteractor(UserRepository userRepository, VerifyUserTemporaryTokenInteractor verifyUserTemporaryTokenInteractor, EmailSenderRepository emailSenderRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
-        this.passwordTokenRepository = passwordTokenRepository;
+        this.verifyUserTemporaryTokenInteractor = verifyUserTemporaryTokenInteractor;
+        this.emailSenderRepository = emailSenderRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User requestResetPassword(String token, String password) throws InvalidTokenException, ResourceNotFoundException {
-        Optional<PasswordToken> passwordTokenOptional = passwordTokenRepository.getPasswordTokenByToken(token);
-
-        if (passwordTokenOptional.isEmpty()) {
-            throw new InvalidTokenException("Invalid password token");
-        }
-
-        PasswordToken passwordToken = passwordTokenOptional.get();
-        passwordTokenRepository.deletePasswordToken(passwordToken);
-
-        if (passwordToken.getExpiresAt().before(new Date())) {
-            throw new InvalidTokenException("Token expired");
-        }
+    public User requestResetPassword(String token, String password) throws InvalidTokenException, ResourceNotFoundException, FailedToSendEmailException {
+        User user = verifyUserTemporaryTokenInteractor.verifyToken(token);
 
         String encodedPassword = passwordEncoder.encode(password);
+        user = userRepository.updateUserPassword(user.getId(), encodedPassword);
 
-        return userRepository.updateUserPassword(passwordToken.getUser().getId(), encodedPassword);
+        emailSenderRepository.sendPasswordChangedEmail(user.getEmail(), user.getFirstName());
+        return user;
     }
 
 }
