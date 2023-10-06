ab96fb2d9c6d320aba5e9c32646f5671a00cfbc6
Luiz Matias <luiz.matias1999@gmail.com>
2023-10-06 04:11:51 -0300

    feat: created password token entity

---
+++ src/main/java/com/luizmatias/findadev/domain/entities/PasswordToken.java
@@ -0,0 +1,53 @@
+package com.luizmatias.findadev.domain.entities;
+
+import java.util.Date;
+
+public class PasswordToken {
+
+    private Long id;
+    private User user;
+    private String token;
+    private Date expiresAt;
+
+    public PasswordToken() {
+    }
+
+    public PasswordToken(Long id, User user, String token, Date expiresAt) {
+        this.id = id;
+        this.user = user;
+        this.token = token;
+        this.expiresAt = expiresAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Date getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Date expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+}
